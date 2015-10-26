07f1d2b22e02028fbc98a8117fffde3f50ec792b
MKergosien <Mary@Kergosien.net>
2015-10-26 12:31:18 -0500

    Initial test commit

--- Mathmagician/Program.cs
+++ Mathmagician/Program.cs
@@ -16,7 +16,7 @@ namespace Mathmagician
             string response = Console.ReadLine();
             if (response == "natural numbers")
             {
-                Console.WriteLine("How many?");
+                Console.WriteLine("How many should I count?");
                 Console.Write(prompt);
                 string response_length = Console.ReadLine();
                 int length;
